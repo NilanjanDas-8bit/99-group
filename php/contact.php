e59dc4801b71b1bb83339b1fbdd79fe1edda750e
NilanjanDas-8bit <rivudas.ind@gmail.com>
2025-04-14 00:17:44 +0530

    Initial commit

---
+++ php/contact.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['Name']);
+    $email   = htmlspecialchars($_POST['Email']);
+    $message = htmlspecialchars($_POST['Message']);
+
+    $to      = "";  // <-- change this to your email
+    $subject = "New message from contact form";
+    $body    = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
